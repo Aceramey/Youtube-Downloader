7fdba889440c56c4d92eb1a129cb52b237d315f7
Aceramey <aceramey02@gmail.com>
2024-06-21 21:33:48 -0400

    added linux support

--- YouTube downloader.py
+++ YouTube downloader.py
@@ -1,4 +1,5 @@
 import os
+import platform
 
 vidFormats = ["mp4", "webm", "mov", "mkv"]
 audFormats = ["mp3", "ogg", "wav"]
@@ -7,18 +8,28 @@ audFormats = ["mp3", "ogg", "wav"]
 try:
     import yt_dlp
 except ImportError:
-    print("yt-dlp not found, installing it for you :)")
-    os.system("cmd /c pip install yt-dlp")
+    if platform.system() == "Windows":
+        print("yt-dlp not found, installing it for you :)")
+        os.system("cmd /c pip install yt-dlp")
+    elif platform.system() == "Linux":
+        print("yt-dlp is not installed, either use a venv, or install it with pip using the --break-system-packages flag")
 
-if os.path.exists(os.getcwd() + "\\" + "downloaded") == False:
+if os.path.exists(os.getcwd() + "\\" + "downloaded") == False and platform.system() == "Windows":
     os.system("cmd /c mkdir \"{0}\"".format(os.getcwd() + "\\downloaded"))
     os.system("cmd /c mkdir \"{0}\"".format(os.getcwd() + "\\downloaded\\Audio"))
     os.system("cmd /c mkdir \"{0}\"".format(os.getcwd() + "\\downloaded\\Video"))
+elif os.path.exists(os.getcwd() + "/" + "downloaded") == False and platform.system() == "Linux":
+    os.system("mkdir \"{0}\"".format(os.getcwd() + "/downloaded"))
+    os.system("mkdir \"{0}\"".format(os.getcwd() + "/downloaded/Audio"))
+    os.system("mkdir \"{0}\"".format(os.getcwd() + "/downloaded/Video"))
 
 
 def download(format, playlist):
     vid = ""
-    command = "cmd /c py -m yt_dlp"
+    if platform.system() == "Windows":
+        command = "cmd /c py -m yt_dlp"
+    elif platform.system() == "Linux":
+        command = "python3 -m yt_dlp"
     while vid == "":
         if playlist == True:
             vid = input("Enter link to playlist, or link to video in the playlist: ")
@@ -37,6 +48,8 @@ def download(format, playlist):
                         command += " --yes-playlist -P \"{0}\\downloaded\\Video\" \"{1}\"".format(os.getcwd(), vid)
                     case "audio":
                         command += " --yes-playlist --audio-format mp3 -x -P \"{0}\\downloaded\\Audio\" \"{1}\"".format(os.getcwd(), vid)
+            if platform.system() == "Linux":
+                command.replace("\\", "/")
             os.system(command)
             
         else:
